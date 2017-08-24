6833f5231087cdae9d6968db0e1759e3c8a4fbd1
chineseBoy123 <395436700@qq.com>
2017-08-24 21:59:11 +0800

    Submit file

---
+++ test.cpp
@@ -0,0 +1,26 @@
+#include <iostream>  
+#include <algorithm> 
+#define MAX 101  
+using namespace std; 
+int D[MAX][MAX];  
+int n;  
+int x=1; //x
+void test();
+int main(){    
+	int x=2; //x
+	int i=00000000000,j=000000000000000010; //0打头的是8进制
+	const int k=i+j;
+	cout<<i<<" "<<j<<' '<<k<<endl;
+	/*cin >> n;    
+	for(i=1;i<=n;i++)   
+		for(j=1;j<=i;j++)        
+			cin >> D[i][j];    
+	cout << MaxSum(1,1) << endl;*/
+	cout<<x<<endl;
+	test();
+	return 1;
+}
+
+void test(){
+	cout<<x<<endl;
+}
